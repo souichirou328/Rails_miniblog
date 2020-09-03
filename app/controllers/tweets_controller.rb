c535db6c32dd7de45a7380182707a092d47f0024
souichirou328 <sou3sou2sou8@gamil.com>
2020-09-03 17:06:57 +0900

    Create screen creation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,12 @@ class TweetsController < ApplicationController
   def new
   end
 
+  def create
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text])
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
 end
